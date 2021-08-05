93156d93ff511dd6e9ab37b3ecc245d3a39e6870
Caio <caio.fsm@gmail.com>
2021-08-04 22:58:47 -0300

    Add project files.

---
+++ App.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chatClient
+{
+    public class App
+    {
+        public App(IClient client, string host, string port)
+        {
+            this.Client = client;
+            Host = host;
+            Port = port;
+        }
+        private IClient Client { get; set; }
+
+        private string Host { get; set; }
+        private string Port { get; set; }
+        public async Task<AppResult> Boot()
+        {
+            var shouldContinue= true;
+
+            showWelcomeMessage();
+            var userName = printPromptAndgetInput();
+            //showMessage($"Conectando ao servidor {Host}:{Port}");
+            var connectionResult= Client.Connect(Host, Port);
+            if (!connectionResult.Success)
+            {
+                showMessage($"Falha ao conectar ao {Host}:{Port}");
+                return AppResult.FalhaAoConectar;
+            }
+
+            showMessage($"Você foi registrado como {userName}. Entrando no canal #general");
+            
+            while (shouldContinue)
+            {
+                var input= printPromptAndgetInput();
+                var result = Parser.parseTextInput(input);
+
+                switch (result.Command)
+                {
+                    case Command.GlobalMessage:
+                        //TODO: Mandar para o servidor
+                        Client.SendMessage(result.Message);
+                        break;
+                    case Command.PrivateMessage:
+                        //TODO: Mandar para o servidor
+                        Client.SendMessage(result.Message, new User { Name = result.UserName });
+                        break;
+                    case Command.Exit:
+                        return AppResult.Sair;
+                        break;
+                    case Command.InvalidCommand:
+                        break;
+                }
+
+                await Task.Delay(100);
+            }
+
+            return 0;
+        }
+
+
+        public void showWelcomeMessage()
+        {
+            showMessage("*** Bem vindo ao chat. Insira seu usuário:");
+        }
+
+        public void showMessage(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public string printPromptAndgetInput()
+        {
+            Console.Write("> ");
+            return Console.ReadLine();
+        }
+    }
+
+    public enum AppResult
+    {
+        FalhaAoConectar = 1,
+        Sair = 2
+    }
+
+    public enum Command
+    {
+        GlobalMessage = 1,
+        PrivateMessage = 2,
+        Exit = 3,
+        InvalidCommand= 4
+    }
+
+
+}
